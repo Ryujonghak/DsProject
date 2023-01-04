ac1d9fdc727c20fb3b2993a5631a70c0fd0f430e
Ryujonghak <hy5849@gmail.com>
2023-01-04 10:13:34 +0900

    백엔드 reservtion 추가

---
+++ BackEnd/src/main/java/com/example/backend/controller/ReservtionController.java
@@ -0,0 +1,12 @@
+package com.example.backend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class ReservtionController {
+
+}
