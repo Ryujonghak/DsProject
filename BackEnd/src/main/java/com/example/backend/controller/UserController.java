13946ab1619729e50aa653589c9b7afb9e94516c
Ryujonghak <hy5849@gmail.com>
2023-01-05 19:01:32 +0900

    유저 삭제 해결

--- BackEnd/src/main/java/com/example/backend/controller/UserController.java
+++ BackEnd/src/main/java/com/example/backend/controller/UserController.java
@@ -313,7 +313,7 @@ public class UserController {
 
     @DeleteMapping("/user/deletion/{id}")
 //    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Object> deleteUser(@PathVariable int id) {
+    public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
 
 //        프론트엔드 쪽으로 상태정보를 보내줌
         try {
