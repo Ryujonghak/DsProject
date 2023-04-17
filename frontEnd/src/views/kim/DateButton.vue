11cc1657d65584ba8c4bb89fa13c9db9662e8466
cheolwon2889 <114125050+cheolwon2889@users.noreply.github.com>
2023-04-18 07:29:31 +0900

    Day컴포넌트 코드 수정 및 날짜 버튼 컴포넌트 생성

---
+++ frontEnd/src/views/kim/DateButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 날짜 버튼 시작 -->
+  <div class="col-md-6 reset-padding" style="padding-right: 20px">
+    <p class="title-text">
+      <!-- {{ year }}년 {{ month }}월 {{ dayNumber }}일 (오늘) -->
+    </p>
+    <!-- 선택되면 3항 연산자로 뒤에 btn2가 붙도록 하면됨. -->
+    <button @click="week('day1')" class="day-btn">
+      <p style="margin-bottom: 10px">오늘</p>
+      <!-- 선택되면 3항 연산자로 뒤에 bg가 붙도록 하면됨. -->
+      <!-- <h4 class="day-h4">{{ dayNumber }}</h4> -->
+    </button>
+    <button class="day-btn2">
+      <p style="margin-bottom: 10px">오늘</p>
+      <!-- <h4 class="day-h4-bg">{{ dayNumber }}</h4> -->
+    </button>
+
+    <!-- 내일 -->
+    <button @click="week('day2')" class="day-btn">
+      <p style="margin-bottom: 10px">내일</p>
+      <!-- <h4 class="day-h4">{{ dayNumber }}</h4> -->
+    </button>
+    <button class="day-btn2">
+      <p style="margin-bottom: 10px">내일</p>
+      <!-- <h4 class="day-h4-bg">{{ dayNumber }}</h4> -->
+    </button>
+  </div>
+  <!-- 닐짜 버튼 끝 -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- frontEnd/src/views/kim/DayCom.vue
+++ frontEnd/src/views/kim/DayCom.vue
@@ -1,33 +1,15 @@
 <template>
   <!-- 날짜, 시간 버튼 시작 -->
   <div class="col-md-12 reset-padding">
-    <!-- 날짜 버튼 시작 -->
-    <div class="col-md-6 reset-padding" style="padding-right: 20px">
-      <p class="title-text">
-        {{ year }}년 {{ month }}월 {{ dayNumber }}일 (오늘)
-      </p>
-      <!-- 선택되면 3항 연산자로 뒤에 btn2가 붙도록 하면됨. -->
-      <button @click="week('day1')" class="day-btn">
-        <p style="margin-bottom: 10px">오늘</p>
-        <!-- 선택되면 3항 연산자로 뒤에 bg가 붙도록 하면됨. -->
-        <h4 class="day-h4">{{ dayNumber }}</h4>
-      </button>
-      <button class="day-btn2">
-        <p style="margin-bottom: 10px">오늘</p>
-        <h4 class="day-h4-bg">{{ dayNumber }}</h4>
-      </button>
-
-      <!-- 내일 -->
-      <button @click="week('day2')" class="day-btn">
-        <p style="margin-bottom: 10px">내일</p>
-        <h4 class="day-h4">{{ dayNumber }}</h4>
-      </button>
-      <button class="day-btn2">
-        <p style="margin-bottom: 10px">내일</p>
-        <h4 class="day-h4-bg">{{ dayNumber }}</h4>
-      </button>
-    </div>
-    <!-- 닐짜 버튼 끝 -->
+    <DateButton
+      v-for="(date, index) in dates"
+      :key="index"
+      :id="index"
+      :year="date.year"
+      :month="date.month"
+      :dayNumber="date.dayNumber"
+      :day="date.day"
+    />
     <!-- 시간 버튼 시작 -->
     <div class="col-md-6 reset-padding">
       <p class="title-text">상영시간</p>
@@ -39,9 +21,13 @@
 </template>
 
 <script>
+import DateButton from "./DateButton.vue";
 export default {
   mounted() {
     this.getDate();
+  },
+  components: {
+    DateButton,
   },
   data() {
     return {
@@ -54,14 +40,7 @@ export default {
         "토요일",
         "일요일",
       ],
-      dates: [
-        {
-          year: "",
-          month: "",
-          dayNumber: "", // 16일
-          day: "", // 월요일= 1 , 화요일= 2 , ... 일요일= 0
-        },
-      ],
+      dates: [],
       dateY: "", // 영화관 + 날짜 = 시간조회로 사용됨
     };
   },
@@ -73,22 +52,26 @@ export default {
         if (date.getDate() === 1) {
           date.setMonth(date.getMonth());
         }
-        this.dates[i] = [
-          (this.year = date.getFullYear()),
-          (this.month = date.getMonth() + 1),
-          (this.dayNumber = date.getDate()),
-          (this.day = date.getDay()),
+        const day = [
+          date.getFullYear(),
+          date.getMonth() + 1,
+          date.getDate(),
+          date.getDay(),
         ];
+
+        console.log(this.dates);
+        this.dates.push(day);
 
         // FIXME: 1. 콘솔로그 찍을때 처음에 다 나옴.
         // FIXME: 2. 날짜에 0을 추가했는데 추가가 안됨. 고쳐야됨.
-        console.log(this.dates);
-        if (this.dates[i].month < 10) {
-          this.dates[i].month = "0" + this.dates[i].month;
-        }
+        // FIXME: 3. 객체배열에 year = ?? 이렇게 담고 싶은데 0으로 담김
+
+        // if (this.dates[i].month < 10) {
+        //   this.dates[i].month = "0" + this.dates[i].month;
+        // }
       }
       this.dateY =
-        String(this.year) + String(this.month) + String(this.dayNumber); // 20230416 이렇게 보냄
+        String(this.year) + String(this.month) + String(this.dayNumber); // 20230416 이렇게 보내야 됨
     },
   },
 };
