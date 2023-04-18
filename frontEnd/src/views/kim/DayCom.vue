3db803d16893558d60aa8034cd4625c58573f8d1
cheolwon2889 <114125050+cheolwon2889@users.noreply.github.com>
2023-04-19 07:45:51 +0900

    Update DayCom.vue

--- frontEnd/src/views/kim/DayCom.vue
+++ frontEnd/src/views/kim/DayCom.vue
@@ -9,6 +9,7 @@
       :month="date.month"
       :dayNumber="date.dayNumber"
       :day="date.day"
+      @select-day="selectedDay()"
     />
     <!-- 시간 버튼 시작 -->
     <div class="col-md-6 reset-padding">
@@ -42,11 +43,19 @@ export default {
       ],
       dates: [],
       dateY: "", // 영화관 + 날짜 = 시간조회로 사용됨
+      year: 0,
+      month: 0,
+      dayNumber: 0,
+      day: 0,
     };
   },
   methods: {
     getDate() {
       let date = new Date();
+      this.year = 0;
+      this.month = 1;
+      this.dayNumber = 2;
+      this.day = 3;
       for (let i = 0; i < 14; i++) {
         date.setDate(date.getDate() + 1);
         if (date.getDate() === 1) {
@@ -57,21 +66,23 @@ export default {
           date.getMonth() + 1,
           date.getDate(),
           date.getDay(),
+          i,
         ];
-
-        console.log(this.dates);
         this.dates.push(day);
-
-        // FIXME: 1. 콘솔로그 찍을때 처음에 다 나옴.
-        // FIXME: 2. 날짜에 0을 추가했는데 추가가 안됨. 고쳐야됨.
-        // FIXME: 3. 객체배열에 year = ?? 이렇게 담고 싶은데 0으로 담김
-
-        // if (this.dates[i].month < 10) {
-        //   this.dates[i].month = "0" + this.dates[i].month;
-        // }
       }
       this.dateY =
         String(this.year) + String(this.month) + String(this.dayNumber); // 20230416 이렇게 보내야 됨
+      // FIXME: 1. 콘솔로그 찍을때 처음에 다 나옴.
+      // FIXME: 2. 날짜에 0을 추가했는데 추가가 안됨. 고쳐야됨.
+      // FIXME: 3. 객체배열에 year = ?? 이렇게 담고 싶은데 0으로 담김
+
+      // if (this.dates[i].month < 10) {
+      //   this.dates[i].month = "0" + this.dates[i].month;
+      // }
+    },
+    selectedDay(id) {
+      console.log(id);
+      // 선택된 날짜 출력하고 active로 css 컨트롤 할꺼임
     },
   },
 };
